10b2cbdc9fcd92f9e06b9bed9f6207ace2386ab7
Lacey Murphy <lneagle@gmail.com>
2025-07-16 11:29:29 -0400

    Added combineUsers

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  }
+
+  args.forEach((arg) => {
+    combinedObject.users.push(...arg);
+  });
+
+  combinedObject.merge_date = Date.today().toString("M/d/yyyy");
+
+  return combinedObject;
+}
 
 
 module.exports = {
